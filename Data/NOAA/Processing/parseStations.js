94065bc651e83b495c6d6395f8c9aeeb2ffb9182
s1ide <trombonedude05@gmail.com>
2022-04-20 16:54:47 -0700

    added NOAA stations

---
+++ Data/NOAA/Processing/parseStations.js
@@ -0,0 +1,28 @@
+const STATION_RAW = "Data/NOAA/Data/relevant-stations.txt";
+const OUTPUT_FILE = "Data/NOAA/Data/stations.json";
+const fs = require("fs");
+
+let stations = [];
+let data = fs.readFileSync(STATION_RAW, "utf8");
+data_sp = data.split("\n");
+data_sp.forEach((elem, i) => {
+    if (elem !== "") {
+        let station = {
+            id: elem.substr(0, 11),
+            location: {
+                lat: Number.parseFloat(elem.substr(12, 8)),
+                long: Number.parseFloat(elem.substr(21, 9)),
+                elev: Number.parseFloat(elem.substr(31, 6)),
+            },
+            state: elem.substr(38, 2),
+            name: elem.substr(41, 30).trim(),
+            WMO_id:
+                elem.substr(80, 5).trim() === ""
+                    ? null
+                    : elem.substr(80, 5).trim(),
+        };
+        stations.push(station);
+    }
+});
+
+fs.writeFileSync(OUTPUT_FILE, JSON.stringify(stations), "utf8");
