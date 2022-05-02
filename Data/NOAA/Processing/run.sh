0aa989c30bbd877ac629564e4870bdff18cffe64
Theofghy <theofghy1289@gmail.com>
2022-05-02 10:11:32 -0700

    GRADLE

--- Data/NOAA/Processing/run.sh
+++ Data/NOAA/Processing/run.sh
@@ -1,8 +1,11 @@
 set -e
-DATA_RAW_FOLDER=Data/NOAA/Data/raw
-STATIONS_JSON=Data/NOAA/Data/stations.json
+ROOT=$(git rev-parse --show-toplevel)
+
+DATA_RAW_FOLDER=${ROOT}/Data/NOAA/Data/raw
+STATIONS_JSON=${ROOT}/Data/NOAA/Data/stations.json
 
 if ! [ -e ${DATA_RAW_FOLDER}/ghcnd-stations.txt ]; then
 	curl -s -o - "https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd-stations.txt" | grep US1CA > ${DATA_RAW_FOLDER}/ghcnd-stations.txt
 fi
 node Data/NOAA/Processing/parseStations.js ${DATA_RAW_FOLDER}/ghcnd-stations.txt ${STATIONS_JSON}
+
