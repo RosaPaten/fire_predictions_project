5bab6f99240c047b14faa0a048b5b3f7fd34f60f
Theofghy <theofghy1289@gmail.com>
2022-05-11 09:25:14 -0700

    update run.sh, parseStations still throws an error

--- Data/NOAA/Processing/run.sh
+++ Data/NOAA/Processing/run.sh
@@ -1,11 +1,13 @@
-set -e
+set -xe
 ROOT=$(git rev-parse --show-toplevel)
 
 DATA_RAW_FOLDER=${ROOT}/Data/NOAA/Data/raw
 STATIONS_JSON=${ROOT}/Data/NOAA/Data/stations.json
 
+mkdir -o ${DATA_RAW_FOLDER}
+
 if ! [ -e ${DATA_RAW_FOLDER}/ghcnd-stations.txt ]; then
 	curl -s -o - "https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd-stations.txt" | grep US1CA > ${DATA_RAW_FOLDER}/ghcnd-stations.txt
 fi
-node Data/NOAA/Processing/parseStations.js ${DATA_RAW_FOLDER}/ghcnd-stations.txt ${STATIONS_JSON}
+node ${ROOT}Data/NOAA/Processing/parseStations.js ${DATA_RAW_FOLDER}/ghcnd-stations.txt ${STATIONS_JSON}
 
